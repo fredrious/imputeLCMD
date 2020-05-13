2eb02c01eed669f068b81d33d93e62e53bce7dee
FarhadSH <farhad.shakeri@uni-bonn.de>
2020-05-13 18:28:19 +0200

    Create impRF.R
    imputation by random forest (package: missForest)

---
+++ R/impRF.R
@@ -0,0 +1,18 @@
+# -------------------------------------------------------------------------------------
+# this function performs missing values imputation based on missForest algorithm
+# -------------------------------------------------------------------------------------
+
+# arguments ___________________________________________________________________________
+#           : dataSet.mvs       - expression matrix with MVs (either peptides or proteins)
+
+# output ______________________________________________________________________________
+#           : dataSet.imputed      - dataset containing complete abundances
+require(missForest)
+impute.wrapper.missForest = function(dataSet.mvs, ...){
+  
+  resultmissForest = missForest(mwk, verbose = TRUE, ...)
+  dataSet.imputed = resultmissForest$ximp
+  
+  return(dataSet.imputed)
+  
+}
